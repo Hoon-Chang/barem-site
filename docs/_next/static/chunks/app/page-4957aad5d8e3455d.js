f5e3e66c23ba21b5cb2c8db796d5f6fdbdfede09
Eddy <skywing@innerbus.com>
2026-09-10 18:36:26 +0700

    Publish static site to docs/ for Pages recovery.
    Include the Google Search Console verification HTML file.

---
+++ docs/_next/static/chunks/app/page-4957aad5d8e3455d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{8604:(e,s,n)=>{Promise.resolve().then(n.bind(n,3352)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[923,441,255,358],()=>e(e.s=8604)),_N_E=e.O()}]);
